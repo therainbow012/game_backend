1af5a73e79b5f03eb9a0578595abe02851dc7bad
rainbow11 <therainbow012@gmail.com>
2023-11-29 20:47:12 +0530

    done.

--- database/migrations/2023_11_27_212710_add_by_reference_code_rainbow_to_users_table.php
+++ database/migrations/2023_11_27_212710_add_by_reference_code_rainbow_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('by_reference_code_rainbow')->after('by_reference_code')->nullable();
+            $table->string('by_reference_code_rainbow')->after('email')->nullable();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('by_reference_code_rainbow')->after('by_reference_code')->nullable();
+            $table->string('by_reference_code_rainbow')->after('email')->nullable();
         });
     }
 };
